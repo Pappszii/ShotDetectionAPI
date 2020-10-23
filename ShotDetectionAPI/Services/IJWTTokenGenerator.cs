c15133d69afa1ef1c75419b07428f4184d36c236
Pappszii <erdelyidanii@gmail.com>
2020-10-23 14:38:03 +0200

    Implemented Token generator method

---
+++ ShotDetectionAPI/Services/IJWTTokenGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ShotDetectionAPI.Services
+{
+    interface IJWTTokenGenerator
+    {
+        string GenerateToken(IdentityUser user);
+    }
+}
---
+++ ShotDetectionAPI/Services/JWTTokenGenerator.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShotDetectionAPI.Services
+{
+    public class JWTTokenGenerator : IJWTTokenGenerator
+    {
+        private readonly IConfiguration _config;
+        public JWTTokenGenerator(IConfiguration config)
+        {
+
+        }
+        public string GenerateToken(IdentityUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.GivenName,user.UserName),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Email,user.Email),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Token:Key"]));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials=creds,
+                Issuer=_config["Token:Issuer"],
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
