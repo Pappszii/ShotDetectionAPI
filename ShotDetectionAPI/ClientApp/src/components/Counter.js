2bc3bb4cd98b00cf4e38f1ba92b976f4d7f62683
Pappszii <erdelyidanii@gmail.com>
2020-10-25 15:11:48 +0100

    added GoogleMaps package added sample map to /components/Counter.js

--- ShotDetectionAPI/ClientApp/src/components/Counter.js
+++ ShotDetectionAPI/ClientApp/src/components/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Map from '../misc/Map';
 
 export class Counter extends Component {
   static displayName = Counter.name;
@@ -25,6 +26,9 @@ export class Counter extends Component {
         <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
 
         <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+        <div>
+        <Map/>
+        </div>
       </div>
     );
   }
---
+++ ShotDetectionAPI/ClientApp/src/misc/Map.js
@@ -0,0 +1,44 @@
+﻿import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import mapsconfig from "../misc/mapsConfig"
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
+function MyComponent() {
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  const key = mapsconfig;
+  return (
+    <LoadScript googleMapsApiKey="{key}">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  );
+}
+
+export default React.memo(MyComponent);
